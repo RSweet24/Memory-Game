7010abeda5c6b62894fdc0acc51cbbf72ea69139
RSweet24 <computergeek14@gmail.com>
2018-01-04 09:14:14 -0600

    Add Click Event to Cards

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 /*
  * List of icons for the cards
  */
-let cards = ['fa-bicycle', 'fa-bullhorn', 'fa-diamond', 'fa-television', 'fa-wifi', 'fa-taxi', 'fa-ship', 'fa-laptop', 
-             'fa-bicycle', 'fa-bullhorn', 'fa-diamond', 'fa-television', 'fa-wifi', 'fa-taxi', 'fa-ship', 'fa-laptop'];
+let cards = ['fa-bicycle', 'fa-bullhorn', 'fa-diamond', 'fa-television', 'fa-wifi', 'fa-taxi', 'fa-ship', 'fa-laptop',
+    'fa-bicycle', 'fa-bullhorn', 'fa-diamond', 'fa-television', 'fa-wifi', 'fa-taxi', 'fa-ship', 'fa-laptop'
+];
 /*
     Shuffling the cards and then creating the html
 */
@@ -13,19 +14,25 @@ function blankCard() {
     let newBlankCard = document.createElement('li');
     newBlankCard.setAttribute('class', 'card');
     return newBlankCard;
-} 
-function cardIcon(i){
+}
+
+function cardIcon(i) {
     let newIcon = document.createElement('i');
     newIcon.setAttribute('class', 'fa ' + cards[i]);
     return newIcon;
 }
-for (var i = 0; i < cards.length; i++){
+for (var i = 0; i < cards.length; i++) {
     deck.appendChild(blankCard(i)).appendChild(cardIcon(i));
+}
+
+function showCard(cardToFlip) {
+    cardToFlip.setAttribute('class', 'card show');
 }
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -49,3 +56,12 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+let clickedCard = document.querySelectorAll('.card'),
+    result;
+for (var i = 0; i < clickedCard.length; i++) {
+    result = clickedCard[i];
+    result.addEventListener('click', function () {
+        showCard(this);
+    });
+}
