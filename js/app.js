51a4bcd54b337fe6e23a101d2ca63f853acadd3c
RSweet24 <computergeek14@gmail.com>
2018-01-10 09:28:17 -0600

    Added function to change CSS of Cards that match.

--- js/app.js
+++ js/app.js
@@ -4,30 +4,12 @@
 let cards = ['fa-bicycle', 'fa-bullhorn', 'fa-diamond', 'fa-television', 'fa-wifi', 'fa-taxi', 'fa-ship', 'fa-laptop',
     'fa-bicycle', 'fa-bullhorn', 'fa-diamond', 'fa-television', 'fa-wifi', 'fa-taxi', 'fa-ship', 'fa-laptop'
 ];
+let shownCards = [];
 /*
-    Shuffling the cards and then creating the html
+    Shuffling the cards
 */
 cards = shuffle(cards);
 let deck = document.querySelector('.deck');
-
-function blankCard() {
-    let newBlankCard = document.createElement('li');
-    newBlankCard.setAttribute('class', 'card');
-    return newBlankCard;
-}
-
-function cardIcon(i) {
-    let newIcon = document.createElement('i');
-    newIcon.setAttribute('class', 'fa ' + cards[i]);
-    return newIcon;
-}
-for (var i = 0; i < cards.length; i++) {
-    deck.appendChild(blankCard(i)).appendChild(cardIcon(i));
-}
-
-function showCard(cardToFlip) {
-    cardToFlip.setAttribute('class', 'card show');
-}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -44,12 +26,53 @@ function shuffle(array) {
 
     return array;
 }
+// creating the html
+function blankCard() {
+    let newBlankCard = document.createElement('li');
+    newBlankCard.setAttribute('class', 'card');
+    return newBlankCard;
+}
+// Adding icon to the cards
+function cardIcon(i) {
+    let newIcon = document.createElement('i');
+    newIcon.setAttribute('class', 'fa ' + cards[i]);
+    return newIcon;
+}
+for (var i = 0; i < cards.length; i++) {
+    deck.appendChild(blankCard(i)).appendChild(cardIcon(i));
+}
+// Flipping the cards when clicked
+function showCard(cardToFlip) {
+    cardToFlip.setAttribute('class', 'card open show');
+    shownCards.push(cardToFlip);
+    if(shownCards.length === 2){
+        cardsToMatch(shownCards);
+        shownCards = [];
+    }
+}
 
+function setCardAsMatch(card){
+    card.setAttribute('class', 'card match');
+}
+
+function cardsToMatch(shownCards){
+    if(shownCards[0].innerHTML === shownCards[1].innerHTML){
+        setCardAsMatch(shownCards[0]);
+        setCardAsMatch(shownCards[1]);
+    }else{
+        function noMatchAnimate(x) {
+            $('.card').addClass(x + ' animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+              $(this).removeClass('shake animated');
+            });
+          };
+        return false;
+    }
+}
 
 /*
  * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - display the card's symbol (put this functionality in another function that you call from this one) - done
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one) - done
  *  - if the list already has another card, check to see if the two cards match
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
@@ -57,11 +80,13 @@ function shuffle(array) {
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
 
+// Click event
 let clickedCard = document.querySelectorAll('.card'),
     result;
 for (var i = 0; i < clickedCard.length; i++) {
     result = clickedCard[i];
     result.addEventListener('click', function () {
         showCard(this);
+        
     });
 }
