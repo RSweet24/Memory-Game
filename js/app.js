0ed3d0de7bdc9719a0b74ac3c5f102b736fdaf3f
Reuben Sweet <computergeek14@gmail.com>
2018-01-13 15:55:56 -0600

    Adds timer to Scoreboard

--- js/app.js
+++ js/app.js
@@ -23,6 +23,7 @@ matchesLeft = 8;
 moves.innerText = moveCounter;
 createCards();
 assignClickHandler();
+timer();
 }
 
 function createCards() {
@@ -139,5 +140,33 @@ function cardClickedEventHandler(cardToShow){
 showCard(cardToShow.target);
     if(matchesLeft === 0){
         alert("You win");
+        clearTimeout(t);
+        stopWatch.textContent = "00:00:00";
+        seconds = 0; minutes = 0; hours = 0;
+        resetGame();
     }
 }
+
+let stopWatch = document.querySelector('.stop-watch')
+    seconds = 0, minutes = 0, hours = 0,
+    t;
+
+function add() {
+    seconds++;
+    if (seconds >= 60) {
+        seconds = 0;
+        minutes++;
+        if (minutes >= 60) {
+            minutes = 0;
+            hours++;
+        }
+    }
+
+    stopWatch.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+
+    timer();
+}
+function timer() {
+    t = setTimeout(add, 1000);
+}
+timer();
