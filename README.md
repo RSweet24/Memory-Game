29ded225c7548c453b7c25a936c0b46659847152
RSweet24 <computergeek14@gmail.com>
2018-01-23 20:01:41 -0600

    Finishing up the project
    Timer works correctly now, Stars are being take away as moves are added, Modal is displaying all the content, Restart Button is restarting the game.

--- README.md
+++ README.md
@@ -3,18 +3,11 @@
 ## Table of Contents
 
 * [Instructions](#instructions)
-* [Contributing](#contributing)
 
 ## Instructions
 
-The starter project has some HTML and CSS styling to display a static version of the Memory Game project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+In case you are not familiar with the Memory Game, The point of the game is to match all of the cards on the Game Board.
 
-To get started, open `js/app.js` and start building out the app's functionality
+You may only turn over 2 cards at a time, If the cards do not match the game board will shake. When they do match they will flip and change to green. A timer will start as soon as the first card is clicked and the more moves it takes you the less stars you get. At the end of the game a modal will appear with your final score and time and star number.
 
-For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
-
-## Contributing
-
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
-
-For details, check out [CONTRIBUTING.md](CONTRIBUTING.md).
+Enjoy the game!
--- js/app.js
+++ js/app.js
@@ -2,155 +2,171 @@
  * List of icons for the cards
  */
 let cards = ['fa-bicycle', 'fa-bullhorn', 'fa-diamond', 'fa-television', 'fa-wifi', 'fa-taxi', 'fa-ship', 'fa-laptop',
-'fa-bicycle', 'fa-bullhorn', 'fa-diamond', 'fa-television', 'fa-wifi', 'fa-taxi', 'fa-ship', 'fa-laptop'
+    'fa-bicycle', 'fa-bullhorn', 'fa-diamond', 'fa-television', 'fa-wifi', 'fa-taxi', 'fa-ship', 'fa-laptop'
 ];
+
 let shownCards = [];
 let deck = document.querySelector('.deck');
 let matchesLeft = 8;
 let moveCounter = 0;
 let moves = document.querySelector('span');
+let timerStarted = false;
+let modal = document.getElementById('youWon');
+let modalClose = document.getElementsByClassName("close")[0];
+let stopWatch = document.querySelector('.stop-watch');
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+let t = null;
+
 document.querySelector('.restart').addEventListener('click', resetGame);
 
 startGame();
+// Setting up the game
+function startGame() {
 
-function startGame(){
-/*
-Shuffling the cards
-*/
-cards = shuffle(cards);
-deck = document.querySelector('.deck');
-matchesLeft = 8;
-moves.innerText = moveCounter;
-createCards();
-assignClickHandler();
-timer();
+    cards = shuffle(cards);
+    deck = document.querySelector('.deck');
+    matchesLeft = 8;
+    moveCounter = 0;
+    moves.innerText = moveCounter;
+    createCards();
+    assignClickHandler();
+    resetTimer();
 }
-
+// Creating layout for cards
 function createCards() {
-for (var i = 0; i < cards.length; i++) {
-    deck.appendChild(blankCard(i)).appendChild(cardIcon(i));
+    for (var i = 0; i < cards.length; i++) {
+        deck.appendChild(blankCard(i)).appendChild(cardIcon(i));
+    }
 }
+// Resetting the game
+function resetGame() {
+    cards = shuffle(cards);
+    matchesLeft = 8;
+    moveCounter = 0;
+    clearCards();
+    startGame();
+    clearTimeout();
+    resetStars();
 }
-
-function resetGame(){
-cards = shuffle(cards);
-matchesLeft = 8;
-clearCards();
-startGame();
-}
-
+// Clearing the cards to blank
 function clearCards() {
-var gameDeck = document.querySelector('.deck');
-gameDeck.innerHTML = '';
+    var gameDeck = document.querySelector('.deck');
+    gameDeck.innerHTML = '';
 }
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-var currentIndex = array.length,
-    temporaryValue, randomIndex;
+    var currentIndex = array.length,
+        temporaryValue, randomIndex;
 
-while (currentIndex !== 0) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-}
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
 
-return array;
+    return array;
 }
 
 // creating the html
 function blankCard() {
-let newBlankCard = document.createElement('li');
-newBlankCard.setAttribute('class', 'card');
-return newBlankCard;
+    let newBlankCard = document.createElement('li');
+    newBlankCard.setAttribute('class', 'card');
+    return newBlankCard;
 }
 
 // Adding icon to the cards
 function cardIcon(i) {
-let newIcon = document.createElement('i');
-newIcon.setAttribute('class', 'fa ' + cards[i]);
-return newIcon;
+    let newIcon = document.createElement('i');
+    newIcon.setAttribute('class', 'fa ' + cards[i]);
+    return newIcon;
 }
 
-// Flipping the cards when clicked
+// Showing the cards
 function showCard(cardToFlip) {
-cardToFlip.setAttribute('class', 'card open show');
-shownCards.push(cardToFlip);
-if(shownCards.length === 2){
-    cardsToMatch(shownCards);
-    shownCards = [];
+    cardToFlip.setAttribute('class', 'card open show');
+    shownCards.push(cardToFlip);
+    if (shownCards.length === 2) {
+        cardsToMatch(shownCards);
+        shownCards = [];
+    }
+    if(matchesLeft === 0){
+        modal.style.display = "block";
+    }
 }
+// Flipping the cards when they match
+function setCardAsMatch(card) {
+    card.setAttribute('class', 'card match animated flip');
+    setTimeout(function () {
+        $(card).removeClass('animated flip');
+    }, 2000);
+
 }
-
-function setCardAsMatch(card){
-card.setAttribute('class', 'card match animated flip');
-setTimeout(function(){
-    $(card).removeClass('animated flip');
-}, 2000);
-
-}
-
+// Shaking the board when cards don't match
 function noMatchAnimate() {
-$('.deck').addClass('animated shake').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
-  $(this).removeClass('animated shake');
-  $('.card').removeClass('open show');
-});
+    $('.deck').addClass('animated shake').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+        $(this).removeClass('animated shake');
+        $('.card').removeClass('open show');
+    });
 };
-
-function cardsToMatch(shownCards){
-if(shownCards[0].innerHTML === shownCards[1].innerHTML){
-    setCardAsMatch(shownCards[0]);
-    setCardAsMatch(shownCards[1]);
-    --matchesLeft;
-    addMoves();
-}else{
-    noMatchAnimate();
-    addMoves();
-    return false;
+// Checking cards for a match and incrementing the moves counter
+function cardsToMatch(shownCards) {
+    if (shownCards[0].innerHTML === shownCards[1].innerHTML) {
+        setCardAsMatch(shownCards[0]);
+        setCardAsMatch(shownCards[1]);
+        --matchesLeft;
+        addMoves();
+    } else {
+        noMatchAnimate();
+        addMoves();
+        return false;
+    }
 }
+// Assigning click handler for each card
+function assignClickHandler() {
+    let clickedCard = document.querySelectorAll('.card'), result;
+    for (let i = 0; i < clickedCard.length; i++) {
+        result = clickedCard[i];
+        result.addEventListener('click', cardClickedEventHandler.bind(this));
+    }
 }
-
-function assignClickHandler(){
-let clickedCard = document.querySelectorAll('.card'), result;
-for (var i = 0; i < clickedCard.length; i++) {
-result = clickedCard[i];
-result.addEventListener('click', cardClickedEventHandler.bind(this));
-}
-}
-
+// move counter
 function addMoves() {
     moveCounter++
     moves.innerText = moveCounter;
+    removeStars();
 }
-/*
-* set up the event listener for a card. If a card is clicked:
-*  - display the card's symbol (put this functionality in another function that you call from this one) - done
-*  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one) - done
-*  - if the list already has another card, check to see if the two cards match
-*    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
-*    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
-*    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
-*    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
-*/
-
-// Click event
-function cardClickedEventHandler(cardToShow){
-showCard(cardToShow.target);
-    if(matchesLeft === 0){
-        alert("You win");
-        clearTimeout(t);
-        stopWatch.textContent = "00:00:00";
-        seconds = 0; minutes = 0; hours = 0;
-        resetGame();
+// When the game is over this will remove the stars depending on how many moves it took
+function removeStars() {
+    if (moveCounter === 15 || moveCounter === 25) {
+        $('.stars li')[0].remove();
     }
 }
+// This will set the stars back to 3 at the beginning of the game
+function resetStars(){
+    $('.stars').empty();
+    for (let i = 0; i < 3; i++){
+        let starHTML = '<li><i class="fa fa-star"></i></li>';
+        $('.stars').append(starHTML);
+    } 
+}
 
-let stopWatch = document.querySelector('.stop-watch')
-    seconds = 0, minutes = 0, hours = 0,
-    t;
-
+// Click event that will start the timer and check when the game will be over.
+function cardClickedEventHandler(cardToShow) {
+    showCard(cardToShow.target);
+    if (!timerStarted) {
+        startTimer();
+    }
+    if (matchesLeft === 0) {
+        clearTimeout(t);
+        modalContent();
+    }
+}
+// Timer function to increment the time
 function add() {
     seconds++;
     if (seconds >= 60) {
@@ -164,9 +180,29 @@ function add() {
 
     stopWatch.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
 
-    timer();
+    startTimer();
 }
-function timer() {
+function startTimer() {
     t = setTimeout(add, 1000);
+    timerStarted = true;
 }
-timer();
+
+function resetTimer() {
+    stopWatch.textContent = "00:00:00";
+    seconds = 0; minutes = 0; hours = 0;
+}
+
+modalClose.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+// Modal content 
+function modalContent(){
+    let stars = $('.stars li').length
+    $('p')[0].innerText = "You won the game!!! It took you " + stopWatch.innerText + ' to complete the board. You did it in ' + moveCounter + ' moves, which results in ' + stars + ' stars' + 'Hit the reset button if you would like to play again';
+}
